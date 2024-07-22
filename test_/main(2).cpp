34e05ddf088370fa9a2994e58af2d2bdf77f8d60
PorkShoulder <145848629+PorkShoulder@users.noreply.github.com>
2024-07-22 22:16:25 +0900

    배열 정적배열중 함수에서 고정값으로 받지 않도록 매개변수를 추가하여, 지정된 배열값을 변경해도 이용가능 배열 값을 유동적으로 받으려면 동적배열으로 작성되어야함.

--- test_/main(2).cpp
+++ test_/main(2).cpp
@@ -170,3 +170,93 @@ using namespace std;
 //{
 //	pX += a;
 //}
+
+//int main()
+//{
+//	const int num = 5; //const가 붙는 이유는 배열에서 요소를 받는게 고정된, 미리 지정된 값이어야함 이는 변해선 안되기 때문에 상수로 받아야함.
+//	int test[num];
+//
+//	cout << num << "확인\n";
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> test[i]; // 여기서 오해가 있음 내가 입력한 값은 5개의 배열의 값임
+//						// 이미 초기에 [5]가 되어있음 
+//	}
+//
+//	for (int j = 0; j < num; j++)
+//	{
+//		cout << j + 1 << "확인2" << test[j] << "끝\n";
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int num = 5;
+//	int test[num];
+//
+//	cout << num << "명의 점수를 입력해주세요\n";
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> test[i];
+//	}
+//
+//	for (int s = 0; s < num - 1; s++)
+//	{
+//		for(int t=s+1; t<num; t++) // t=s+1인 이유는 배열이 s다음번째와 비교하기 위해서 그리고 총 배열의 크기 5를 넘지 않는 선에서 여기서는 배열 순서임
+//		{	//if문은 스왑용임. 그리고 이아래는 배열 크기값임.
+//			if (test[t] > test[s]) // s는 test[]배열에서 첫번째이고t는 그 다음
+//			{					   // t와s의 크기를 비교함
+//				int tmp = test[t]; // 비교후 if조건이 성립시 tmp에 t를 보관 
+//				test[t] = test[s]; // s의 값을 t에 전달
+//				test[s] = tmp;	   // 이후 기존tmp에 있던 값을 s에 전달 ->t와 s의 값이 서로 스왑
+//			}	
+//		}
+//	}
+//	for (int j = 0; j < num; j++)
+//	{
+//		cout << j + 1 << "번 점수는" << test[j] << "입니다\n";
+//	}
+//	return 0;
+//
+//}
+
+// 예제 풀기 (8)
+// 예제 1번 답안
+
+// 5개의 요소를 가진 배열을 받아서 최댓값을 리턴하기 -> 값5개 입력 후 최댓값 출력
+
+//int max(int x[], int y);
+//
+//int main()
+//{
+//	const int num = 5;
+//	int array[num];
+//	cout << num <<"명의 시험 점수를 입력하십심오.\n";
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> array[i];
+//	}
+//
+//	int _max = max(array, num);
+//
+//	cout << _max;
+//
+//	return 0;
+//}
+//// max정의
+//int max(int x[], int y)
+//{
+//	int a = x[0];
+//	for (int j = 1; j < y; j++)
+//	{
+//		if (x[j] > a)
+//		{
+//			a = x[j];
+//		}
+//	}
+//	return a;
+//}
